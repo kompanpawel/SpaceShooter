ce79d1eb42109c1b6f12220b21b69d5a3979319c
kompanpawel <pawelmiskiewicz14@wp.pl>
2018-03-23 21:17:06 +0100

    Initialized project

---
+++ desktop/src/pl/kompanpawel/spaceshoooter/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package pl.kompanpawel.spaceshoooter.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import pl.kompanpawel.spaceshoooter.SpaceShoooter;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new SpaceShoooter(), config);
+	}
+}
