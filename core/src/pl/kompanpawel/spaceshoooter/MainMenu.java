549d6ac3abef2963f8aed1a2ad69e50cee27e690
kompanpawel <pawelmiskiewicz14@wp.pl>
2018-03-26 00:22:46 +0200

    v0.2 - added menu and active menu button

--- core/src/pl/kompanpawel/spaceshoooter/MainMenu.java
+++ core/src/pl/kompanpawel/spaceshoooter/MainMenu.java
@@ -2,29 +2,71 @@ package pl.kompanpawel.spaceshoooter;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.assets.AssetManager;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.scenes.scene2d.*;
+import com.badlogic.gdx.scenes.scene2d.ui.Label;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
+import com.badlogic.gdx.utils.viewport.StretchViewport;
 
 public class MainMenu implements Screen {
+
+    private Stage stage;
+    private Table table;
+    private TextButton playButton;
+
+    Texture background;
+
 
     SpaceShoooter game;
 
     public MainMenu(SpaceShoooter game){
         this.game = game;
+
     }
 
     @Override
     public void show() {
+        background = new Texture("starfield.png");
+        stage = new Stage(new StretchViewport(Gdx.graphics.getWidth(), Gdx.graphics.getHeight()));
 
+        playButton = new TextButton("Play",  game.assetManager.get("uiskin.json", Skin.class));
+        playButton.addListener(new ChangeListener() {
+            @Override
+            public void changed(ChangeEvent event, Actor actor) {
+                game.setScreen(new GameScreen(game));
+            }
+        });
+        table = new Table(game.assetManager.get("uiskin.json", Skin.class));
+
+        table.setFillParent(true);
+        Label title = new Label("SpaceShoooter", game.assetManager.get("uiskin.json", Skin.class));
+        //table.debug();
+        table.add(title).row();
+        table.add(playButton).expand().width(400).height(50);
+        table.setTouchable(Touchable.childrenOnly);
+        Gdx.input.setInputProcessor(stage);
+        stage.addActor(table);
     }
 
     @Override
     public void render(float delta) {
-        Gdx.gl.glClearColor(1,0,0,1);
+        Gdx.gl.glClearColor(0,0,0,1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
         game.batch.begin();
-
+        stage.act(delta);
+        stage.draw();
+        //game.batch.draw(background, 0,0);
         game.batch.end();
+
     }
 
     @Override
--- core/src/pl/kompanpawel/spaceshoooter/SpaceShoooter.java
+++ core/src/pl/kompanpawel/spaceshoooter/SpaceShoooter.java
@@ -1,14 +1,21 @@
 package pl.kompanpawel.spaceshoooter;
 
 import com.badlogic.gdx.Game;
+import com.badlogic.gdx.assets.AssetManager;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+
 public class SpaceShoooter extends Game {
 	public SpriteBatch batch;
+	public AssetManager assetManager;
 	
 	@Override
 	public void create () {
         batch = new SpriteBatch();
-        this.setScreen(new GameScreen(this));
+		assetManager = new AssetManager();
+		assetManager.load("uiskin.json", Skin.class);
+		assetManager.finishLoading();
+		this.setScreen(new MainMenu(this));
 	}
 /*
 	public void update() {
