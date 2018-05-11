82b3bdea69409dad39cf7e960df33ac9ce0bb201
kompanpawel <pawelmiskiewicz14@wp.pl>
2018-05-11 11:24:09 +0200

    v0.3.8 - small refactor, moving background

--- core/src/pl/kompanpawel/spaceshoooter/Laser.java
+++ core/src/pl/kompanpawel/spaceshoooter/Laser.java
@@ -1,7 +1,6 @@
 package pl.kompanpawel.spaceshoooter;
 
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.Vector2;
